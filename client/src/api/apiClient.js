731934380433a83932074c0d0d174febe6b8b7ff
Sohail khan <120656530+sohail-68@users.noreply.github.com>
2025-09-08 18:38:02 +0530

    Update apiClient.js

--- client/src/api/apiClient.js
+++ client/src/api/apiClient.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'http://localhost:5000/api', // Replace with your backend URL
+  baseURL: 'https://lmsb-apt8.onrender.com/api', // Replace with your backend URL
   headers: {
     Authorization: `Bearer ${localStorage.getItem('token')}`,
   },
